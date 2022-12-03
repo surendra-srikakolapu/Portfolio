3b3db9927c1586572701ab97f50083eec6aef4dc
surendra-srikakolapu <surendra.srikakolapu@gmail.com>
2022-12-03 12:28:51 +0530

    fourth commit

--- Pythonlogics/settings.py
+++ Pythonlogics/settings.py
@@ -145,10 +145,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'surendra.srikakolapu1@gmail.com'
-EMAIL_HOST_PASSWORD = 'awkxcefwylivniwf'
