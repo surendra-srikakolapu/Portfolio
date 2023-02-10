faf85a924b696c53ef0eb4d0e807a91757a119a0
surendra-srikakolapu <surendra.srikakolapu@gmail.com>
2023-02-10 12:48:53 +0530

    email update

--- Pythonlogics/settings.py
+++ Pythonlogics/settings.py
@@ -145,3 +145,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
 
+
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = '127.0.0.1'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
